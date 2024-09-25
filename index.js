df22c664f097fe4f2c45678d32e7cf1e8f1696c4
sharonodongo <sharon94odongo@gmail.com>
2024-09-25 07:54:04 +0300

    submitting assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+// divide function
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // square function
+  const square = x => x * x;
+  
+  // Other functions (like foo, add, greet, etc.)
+  const foo = function() {
+    return 'bar';
+  }
+  
+  const add = (parameter1, parameter2) => parameter1 + parameter2;
+  
+  // Function Declaration
+  function greet(name) {
+    return `Hello, ${name}!`;
+  }
+  
+  const nums = [1, 2, 3];
+  const squares = nums.map(x => x ** 2);  // Returns [1, 4, 9]
